bc20746bc7273ba35b19f9aba5d39b8859ef0983
Paul_dong <xiaodong0525@163.com>
2017-07-25 14:10:38 +0800

    how to initlize  property of class
    how to initlize  property of class

--- src/main/java/com/java/basic/tech/InstanceProperty.java
+++ src/main/java/com/java/basic/tech/InstanceProperty.java
@@ -4,45 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * It shows a syntax of java. How to initialise the property of class, and it is
- * different from our normal cognition
+ * It shows a syntax of java. How to initialise the property of class, and it is different from our
+ * normal cognition
  * 
  * @author paul
  *
  */
 public class InstanceProperty {
 
-	// it is block of class
-	{
-		System.out.println("InstanceProperty block_01....");
-	}
+    // it is block of class
+    {
+        System.out.println("InstanceProperty block_01....");
+    }
 
-	private List<String> list = new ArrayList<String>() {
-		{ // this is a block of perperty
-			System.out.println("I am in property  block....");
-			add("block_01");
-			add("block_02");
-			add("block_03");
+    private List<String> list = new ArrayList<String>() {
+        { // this is a block of perperty
+            System.out.println("I am in property  block....");
+            add("block_01");
+            add("block_02");
+            add("block_03");
 
-		}
-	}; // pay attention to semicolon
+        }
+    }; // pay attention to semicolon
 
-	// it is block of class. Pay attention to sequence of execution
-	{
-		System.out.println("InstanceProperty block_02....");
-	}
+    // it is block of class. Pay attention to sequence of execution
+    {
+        System.out.println("InstanceProperty block_02....");
+    }
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		System.out.println("***** Test InstanceProperty begin *****");
+        System.out.println("***** Test InstanceProperty begin *****");
 
-		InstanceProperty test = new InstanceProperty();
+        InstanceProperty test = new InstanceProperty();
 
-		for (int i = 0, size = test.list.size(); i < size; i++) {
-			System.out.println(test.list.get(i));
-		}
+        for (int i = 0, size = test.list.size(); i < size; i++) {
+            System.out.println(test.list.get(i));
+        }
 
-		System.out.println("***** Test InstanceProperty end *****");
-	}
+        System.out.println("***** Test InstanceProperty end *****");
+    }
 
 }
